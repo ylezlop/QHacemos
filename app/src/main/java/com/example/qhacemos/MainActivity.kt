54ef228c85f0441f182aefebc941e6c12fd3d75f
yael <lezama.yael.c@gmail.com>
2026-04-16 23:35:16 -0600

    Commit inicial

---
+++ app/src/main/java/com/example/qhacemos/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.qhacemos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.qhacemos.pantallas.PantallaPrincipal
+import com.example.qhacemos.ui.theme.QHacemosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            QHacemosTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    // Llamamos a la función composable que importamos del nuevo paquete
+                    PantallaPrincipal()
+                }
+            }
+        }
+    }
+}
